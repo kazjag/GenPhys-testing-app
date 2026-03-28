92ea02d9cd1524db8d22315c3f796665f9cb3003
kazjag <91073880+kazjag@users.noreply.github.com>
2026-03-29 00:25:22 +0300

    added feedback page created models Feedback, Problem

--- genphysapp/mainapp/models.py
+++ genphysapp/mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    text = models.TextField() #текст отзыва
+    created_date = models.DateTimeField(auto_now_add=True) #дата написания
+    author = models.CharField(max_length=50) #автор
+
+class Problem(models.Model):
+    task_text = models.TextField()
+    left_border = models.DecimalField(decimal_places=5, max_digits=10)
+    right_border = models.DecimalField(decimal_places=5, max_digits=10)
+    answer = models.CharField(max_length=150)
+    commentary = models.CharField(max_length=300, default = "")
--- genphysapp/mainapp/views.py
+++ genphysapp/mainapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Feedback
+
 
 # Create your views here.
 def main_page(request):
     return render(request, 'mainapp/main_page.html')
+
+
+def render_feedback(request):
+    feedbacks = Feedback.objects.all().order_by("-created_date")
+    return render(request, 'mainapp/feedback_list.html', {'feedbacks': feedbacks})
